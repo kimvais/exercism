f66c862180975a7917ebff7c256ade35181d81ef
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-25 11:32:21 +0200

    Solve raindrops

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -1,3 +1,26 @@
 ﻿module Raindrops
 
-let convert (number: int): string = failwith "You need to implement this function."
+let getSound number =
+    match number with
+    | 3 -> "Pling"
+    | 5 -> "Plang"
+    | 7 -> "Plong"
+    | _ -> ""
+
+let checkIfSome option x =
+    match x with
+    | None -> false
+    | Some _ -> true
+
+let getSounds number =
+    seq {
+        for divisor in [| 3; 5; 7 |] do
+            if number % divisor = 0 then yield getSound divisor
+    }
+
+let convert (number: int): string =
+    let sound = getSounds number |> String.concat ""
+    if sound = "" then
+        sprintf "%d" number
+    else
+        sound
