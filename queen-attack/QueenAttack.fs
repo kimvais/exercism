ce0eacb596df7b722d7f62008a8293f361b5bd22
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-26 11:03:07 +0200

    Slightly cleaner version for Queen Attack

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -2,16 +2,18 @@
 
 open System.Buffers
 
-let create (position: int * int) = 
-    let (x, y) = position
-    if x < 0 || y < 0 || x > 7 || y > 7 then false
-    else true
-    
+let create (position: int * int) =
+    let invalid z =
+        z < 0 || z > 7
+    match position with
+    | (x, y) when invalid x || invalid y -> false
+    | _ -> true
+
 let canAttack (queen1: int * int) (queen2: int * int) =
     let (x1, y1) = queen1
     let (x2, y2) = queen2
-    let xdist = abs(x1 - x2)
-    let ydist = abs(y1 - y2)
+    let xdist = abs (x1 - x2)
+    let ydist = abs (y1 - y2)
     if x1 = x2 then true
     elif y1 = y2 then true
     elif xdist = ydist then true
