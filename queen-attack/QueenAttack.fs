b6d0d93a73bdd858d61ff9155cb3bf05d3978f36
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-26 10:57:19 +0200

    Queen Attack, stupid version.

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,5 +1,18 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+open System.Buffers
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let create (position: int * int) = 
+    let (x, y) = position
+    if x < 0 || y < 0 || x > 7 || y > 7 then false
+    else true
+    
+let canAttack (queen1: int * int) (queen2: int * int) =
+    let (x1, y1) = queen1
+    let (x2, y2) = queen2
+    let xdist = abs(x1 - x2)
+    let ydist = abs(y1 - y2)
+    if x1 = x2 then true
+    elif y1 = y2 then true
+    elif xdist = ydist then true
+    else false
