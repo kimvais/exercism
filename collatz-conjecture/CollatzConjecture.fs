6daef10541ce96f25c63bc94f1d49cb7d1489ff0
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-28 12:02:00 +0200

    Solve Collatz conjencture

--- collatz-conjecture/CollatzConjecture.fs
+++ collatz-conjecture/CollatzConjecture.fs
@@ -1,3 +1,12 @@
 ﻿module CollatzConjecture
 
-let steps (number: int): int option = failwith "You need to implement this function."
+let calcNext num =
+    match num with
+    | 1 -> None
+    | n when n % 2 = 0 -> Some(num, n / 2)
+    | n -> Some(num, 3 * n + 1)
+
+let steps (number: int): int option =
+    match number with
+    | n when n > 0 -> Some (Seq.unfold calcNext number |> Seq.length)
+    | _ -> None
