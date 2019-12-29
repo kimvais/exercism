aa930737c93ccf071d7b3bbfc678675450b7a7c6
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 16:30:45 +0200

    Naive sieve.

--- sieve/Sieve.fs
+++ sieve/Sieve.fs
@@ -1,3 +1,20 @@
 ﻿module Sieve
 
-let primes limit = failwith "You need to implement this function."
+let primes limit =
+    let inner lim =
+        seq {
+
+            let mutable eliminated = Set.empty<int>
+
+
+            let eliminate number =
+                for multiple in [ 2 * number .. number .. lim ] do
+                    eliminated <- eliminated.Add multiple
+                eliminated |> Set.filter (fun n -> n > number)
+
+            for candidate in [ 2 .. lim ] do
+                if not (eliminated.Contains candidate) then
+                    eliminated <- eliminate candidate
+                    yield candidate
+        }
+    inner limit |> Seq.toList
