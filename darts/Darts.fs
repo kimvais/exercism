ce680efb83e45431ad56727b9a8568453ad87176
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 15:25:13 +0200

    Solved darts.

--- darts/Darts.fs
+++ darts/Darts.fs
@@ -1,3 +1,8 @@
 ﻿module Darts
 
-let score (x: double) (y: double): int = failwith "You need to implement this function."
+let score (x: double) (y: double): int =
+    match sqrt (x ** 2.0 + y ** 2.0) with
+    | d when d <= 1.0 -> 10
+    | d when d <= 5.0 -> 5
+    | d when d <= 10.0 -> 1
+    | _ -> 0
