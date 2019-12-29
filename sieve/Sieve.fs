f42685bb202be977b8a5a8272e87b75e5042d552
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 17:15:08 +0200

    Nicer solution.

--- sieve/Sieve.fs
+++ sieve/Sieve.fs
@@ -1,20 +1,27 @@
 ﻿module Sieve
 
-let primes limit =
-    let inner lim =
-        seq {
+type State =
+    | Prime
+    | Composite
 
-            let mutable eliminated = Set.empty<int>
+let eliminate (state: State array) candidate limit =
+    [ 2 * candidate .. candidate .. limit ] |> Seq.iter (fun i -> state.[i] <- Composite)
 
+let findNextCandidate (state: State array) candidate limit =
+    [ candidate + 1 .. limit ]
+    |> Seq.skipWhile (fun idx -> state.[idx] = Composite)
+    |> Seq.tryHead
 
-            let eliminate number =
-                for multiple in [ 2 * number .. number .. lim ] do
-                    eliminated <- eliminated.Add multiple
-                eliminated |> Set.filter (fun n -> n > number)
+let doSieve limit =
+    seq {
+        let candidates = Array.create (limit + 1) Prime
+        let mutable candidate = 2
+        while candidate <= limit && candidate > 0 do
+            yield candidate
+            eliminate candidates candidate limit
+            match findNextCandidate candidates candidate limit with
+            | None -> candidate <- -1
+            | Some n -> candidate <- n
+    }
 
-            for candidate in [ 2 .. lim ] do
-                if not (eliminated.Contains candidate) then
-                    eliminated <- eliminate candidate
-                    yield candidate
-        }
-    inner limit |> Seq.toList
+let primes limit = doSieve limit |> Seq.toList
