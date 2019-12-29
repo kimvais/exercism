9eacbb85d4209c32e7f7d5c9161df4adfeb67f79
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 12:12:20 +0200

    Robot name, partial solution (does not handle collisions)

---
+++ robot-name/RobotName.fs
@@ -0,0 +1,19 @@
+﻿module RobotName
+
+let rnd = System.Random()
+let letters = [ 'A' .. 'Z' ]
+
+type Robot =
+    { id: int }
+
+let mkRobot() =
+    { id = rnd.Next(1, 26 * 26 * 1000) }
+
+let name robot =
+    let a = letters.[robot.id / 1000 % 26]
+    let b = letters.[robot.id / 26000 % 26]
+    let n = robot.id % 1000
+    sprintf "%c%c%03d" a b n
+
+let reset robot =
+    mkRobot()
