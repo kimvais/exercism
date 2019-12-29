99cb8f5690d92a746d2a6d539ecd5bf8b519497e
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 18:10:43 +0200

    Implemented ISBN verifier.

--- isbn-verifier/IsbnVerifier.fs
+++ isbn-verifier/IsbnVerifier.fs
@@ -1,3 +1,22 @@
 ﻿module IsbnVerifier
 
-let isValid isbn = failwith "You need to implement this function."
+open System.Text.RegularExpressions
+
+let isValid (isbn: string): bool =
+    let cleanedUpIsbn = isbn |> Seq.filter (fun c -> c <> '-')
+    match Regex.IsMatch(cleanedUpIsbn |> System.String.Concat, "^[0-9]{9}[0-9|X]$") with
+    | true ->
+        let calcDigit (acc: int, pos: int) (n: char) =
+            let digit =
+                match n with
+                | 'X' -> 10
+                | n -> n |> (string >> int)
+            (acc + pos * digit, pos + 1)
+
+        let checksum, _ =
+            cleanedUpIsbn
+            |> Seq.rev
+            |> Seq.fold calcDigit (0, 1)
+
+        checksum % 11 = 0
+    | false -> false
