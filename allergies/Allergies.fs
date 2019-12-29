cca05f82b96bb0caa76d99c65e2d7f6a7f02e69f
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 14:20:47 +0200

    Allergies.

---
+++ allergies/Allergies.fs
@@ -0,0 +1,23 @@
+﻿module Allergies
+
+open System
+
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let allergicTo codedAllergies (allergen: Allergen) =
+    let a = int allergen
+    codedAllergies &&& a = a
+
+let list codedAllergies =
+    Enum.GetValues(typeof<Allergen>)
+    |> Seq.cast<Allergen>
+    |> Seq.filter (fun a -> allergicTo codedAllergies a)
+    |> Seq.toList
