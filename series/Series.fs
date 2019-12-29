ec907ebc6c55cdc9e794555bc457d961a295bd75
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-29 17:28:36 +0200

    Series.

---
+++ series/Series.fs
@@ -0,0 +1,12 @@
+﻿module Series
+
+let slices (str: string) length =
+    match length with
+    | l when l > 0 ->
+        match str
+              |> Seq.windowed length
+              |> (Seq.map System.String.Concat)
+              |> Seq.toList with
+        | s when s.IsEmpty -> None
+        | s -> Some s
+    | _ -> None
