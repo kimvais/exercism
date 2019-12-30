4dd5f8a196cf8bf9f3624b4a1785cb497dd05eef
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-30 09:35:38 +0200

    Perfect numbers.

---
+++ perfect-numbers/PerfectNumbers.fs
@@ -0,0 +1,20 @@
+﻿module PerfectNumbers
+
+type Classification =
+    | Perfect
+    | Abundant
+    | Deficient
+
+let factors n =
+    [ 1 .. n / 2 ] |> Seq.filter (fun i -> n % i = 0)
+
+let alinquot n = factors n |> Seq.sum
+
+let classify n: Classification option =
+    match n with
+    | n when n < 1 -> None
+    | _ ->
+        match alinquot n with
+        | a when a = n -> Some Perfect
+        | a when a > n -> Some Abundant
+        | _ -> Some Deficient
