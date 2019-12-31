d4f82d9904a73586284ed408b7d84729c10e7030
Kimmo Parviainen-Jalanko <k@77.fi>
2019-12-31 11:24:49 +0200

    Implemented Diffie-Hellman.

--- diffie-hellman/DiffieHellman.fs
+++ diffie-hellman/DiffieHellman.fs
@@ -1,7 +1,38 @@
 ﻿module DiffieHellman
 
-let privateKey primeP = failwith "You need to implement this function."
+open System.Numerics
+open System.Security.Cryptography
 
-let publicKey primeP primeG privateKey = failwith "You need to implement this function."
+let rng = RNGCryptoServiceProvider()
 
-let secret primeP publicKey privateKey = failwith "You need to implement this function."
+// Modular exponentiation taken from https://rosettacode.org/wiki/Modular_exponentiation#F.23
+let expMod a b n =
+    let rec loop a b c =
+        if b = 0I then
+            c
+        else
+            loop (a * a % n) (b >>> 1)
+                (if b &&& 1I = 0I then c
+                 else c * a % n)
+    loop a b 1I
+
+let getRandBytes n =
+    let byteArray = Array.init (n) (fun _ -> (byte) 0)
+    rng.GetBytes byteArray
+    byteArray
+
+let getRandBigInt (max: bigint) =
+    let size = max.GetByteCount()
+    let bytes = getRandBytes size
+    bigint bytes
+    |> abs
+    |> (fun x -> x % max)
+
+let privateKey (primeP: BigInteger) =
+    getRandBigInt primeP
+
+let publicKey primeP primeG privateKey =
+    expMod primeG privateKey primeP
+
+let secret primeP publicKey privateKey =
+    expMod publicKey privateKey primeP
