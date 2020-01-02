094d4ce1b1df8526e7c3151077356e9b15225385
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-02 11:55:04 +0200

    Cleaner way to convert digits to numbers.

--- armstrong-numbers/ArmstrongNumbers.fs
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -3,8 +3,7 @@
 let isArmstrongNumber (number: int): bool =
     let ns = number.ToString()
     let toPow = ns.Length
-    let getDigit c =
-        int c - int '0'
+    let getDigit = string >> int
     ns
     |> Seq.map getDigit
     |> Seq.fold (fun x y -> x + pown y toPow) 0 = number
