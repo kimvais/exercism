df3097aa6d7a08b18b703c4ea3b1935b748584ae
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-03 00:23:52 +0200

    Grains.

---
+++ grains/Grains.fs
@@ -0,0 +1,14 @@
+﻿module Grains
+
+open System.Numerics
+
+let squareGrains n =
+    BigInteger.Pow(2I, (n - 1)) 
+    
+let square (n: int): Result<uint64,string> =
+    match n with
+    | n when n > 0 && n < 65 -> Ok (squareGrains n |> uint64)
+    | _ ->  Error "square must be between 1 and 64"
+    
+let total: Result<uint64,string> =
+    Ok ((squareGrains 65) - 1I |> uint64)
