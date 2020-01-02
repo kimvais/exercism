195147e96aba5c68fbed2e5c1c98b35a6e367d90
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-02 11:51:40 +0200

    Solved Armstrong numbers.

---
+++ armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,10 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool =
+    let ns = number.ToString()
+    let toPow = ns.Length
+    let getDigit c =
+        int c - int '0'
+    ns
+    |> Seq.map getDigit
+    |> Seq.fold (fun x y -> x + pown y toPow) 0 = number
