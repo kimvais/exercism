27b6d556ece7e425e1a18fe6cf8f766511e86a8d
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-03 01:31:21 +0200

    High scores.

---
+++ high-scores/HighScores.fs
@@ -0,0 +1,14 @@
+﻿module HighScores
+
+let scores (values: int list): int list = values
+
+let latest (values: int list): int =
+    values |> List.last
+
+let personalBest (values: int list): int =
+    values |> List.max
+
+let personalTopThree (values: int list): int list =
+    values |> List.sort |> List.rev |> function
+        | n when n |> List.length > 3 -> n |> List.take 3
+        | n -> n
