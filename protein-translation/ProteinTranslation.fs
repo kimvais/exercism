ca059f6a9b787122763b60f0c7416863f6f413d5
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-03 01:05:13 +0200

    Solved protein translation.

--- protein-translation/ProteinTranslation.fs
+++ protein-translation/ProteinTranslation.fs
@@ -1,3 +1,23 @@
 ﻿module ProteinTranslation
 
-let proteins rna = failwith "You need to implement this function."
+type Protein = | Cysteine | Leucine | Methionine | Phenylalanine | Serine | Tryptophan | Tyrosine | STOP
+
+let getProtein codon =
+    match codon with
+    | "AUG" -> Methionine
+    | "UUU" | "UUC" -> Phenylalanine
+    | "UUA" | "UUG" -> Leucine
+    | "UCU" | "UCC" | "UCA" | "UCG" -> Serine
+    | "UAU" | "UAC" -> Tyrosine
+    | "UGU" | "UGC" -> Cysteine
+    | "UGG" -> Tryptophan
+    | "UAA" | "UAG" | "UGA" -> STOP
+    | _ -> failwith (sprintf "Unknown codon %A" codon)
+
+let proteins (rna: string) =
+    rna
+    |> Seq.chunkBySize 3
+    |> Seq.map (fun s -> s |> System.String.Concat |> getProtein)
+    |> Seq.takeWhile (fun p -> p <> STOP)
+    |> Seq.map (fun c -> c.ToString())
+    |> Seq.toList
