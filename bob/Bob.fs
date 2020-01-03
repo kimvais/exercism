e904406853da7b8b50933d597609fad13098a0cd
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 00:01:58 +0200

    Fixed bob.

--- bob/Bob.fs
+++ bob/Bob.fs
@@ -1,20 +1,24 @@
 ﻿module Bob
 
-open System.Text.RegularExpressions
+open System
 
 let stripChars text (chars: string) =
     Array.fold (fun (s: string) c -> s.Replace(c.ToString(), "")) text (chars.ToCharArray())
 
-let Silence = Regex "^\w*$"
-let Yelling = Regex "\w*[^A-Z]+\w*$"
-let Question = Regex ".*\?\w*$"
-let YelledQuestion = Regex "\w*[A-Z]+\w*\?$"
+let (|YelledQuestion|Question|Yelling|Statement|Silence|) (phrase: string) =
+    match phrase with
+    | x when x |> Seq.isEmpty -> Silence
+    | x when x.ToUpper() = x && (x |> Seq.exists Char.IsLetter) ->
+        match x with
+        | y when Seq.last y = '?' -> YelledQuestion
+        | _ -> Yelling
+    | x when Seq.last x = '?' -> Question
+    | _ -> Statement
 
 let response (input: string): string =
-    let text = stripChars input "\n"
-    match text with
-    | x when YelledQuestion.IsMatch(x) -> "Calm down, I know what I'm doing!"
-    | x when Question.IsMatch(x) -> "Sure."
-    | x when Yelling.IsMatch(x) -> "Whoa, chill out!"
-    | x when Silence.IsMatch(x) -> "Fine. Be that way!"
-    | _ -> "Whatever."
+    match (input |> String.filter (Char.IsWhiteSpace >> not)) with
+    | YelledQuestion -> "Calm down, I know what I'm doing!"
+    | Yelling -> "Whoa, chill out!"
+    | Question -> "Sure."
+    | Statement -> "Whatever."
+    | Silence -> "Fine. Be that way!"
