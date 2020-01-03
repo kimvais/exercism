bd303f2ea8efcb8419c92300dcbc1398b8548542
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-03 12:13:29 +0200

    Isogram.

---
+++ isogram/Isogram.fs
@@ -0,0 +1,7 @@
+﻿module Isogram
+
+open System
+
+let isIsogram (str: string) =
+    let word = str.ToLower() |> String.filter Char.IsLower
+    (word |> Seq.distinct |> Seq.length) = (word |> String.length)
