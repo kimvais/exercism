633bc8ca7560ebf5067bc70562501deeb87c251a
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-03 12:01:36 +0200

    Solve acronym without Regex.

---
+++ acronym/Acronym.fs
@@ -0,0 +1,22 @@
+﻿module Acronym
+
+open System
+
+let isDelim = function
+    | ' ' | '_' | '-' -> true
+    | _ -> false
+
+let isAcronymChar = function
+    | c when c >= 'A' && c <= 'Z' -> true
+    | _ -> false
+
+let matchTla t =
+    let (d, c) = t
+    (isDelim d) && (isAcronymChar c)
+
+let abbreviate (s:string) =
+    sprintf " %s" (s.ToUpper())
+    |> Seq.pairwise
+    |> Seq.filter matchTla
+    |> Seq.map (fun (_, c) -> c)
+    |> String.Concat
