23ae7f71250b7e14901eff16c645d6bf570c4c56
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-03 20:28:22 +0200

    3 more tests failing...

--- variable-length-quantity/VariableLengthQuantity.fs
+++ variable-length-quantity/VariableLengthQuantity.fs
@@ -1,8 +1,5 @@
 ﻿module VariableLengthQuantity
 
-open System
-open System
-
 let intToBinary (i: uint32) =
     match i with
     | 0u -> None
@@ -25,21 +22,48 @@ let intToSeqOf7BitInts (s: uint32) =
 let encode (numbers: uint32 list) =
     seq {
         for n in numbers do
-            let s = intToSeqOf7BitInts n |> Seq.rev |> Seq.toList
+            let s =
+                intToSeqOf7BitInts n
+                |> Seq.rev
+                |> Seq.toList
             match s with
-            | last :: rest -> yield (rest |> List.rev |> List.map (fun i -> i ||| 0x80uy)) @ [ last ]
+            | last :: rest ->
+                yield (rest
+                       |> List.rev
+                       |> List.map (fun i -> i ||| 0x80uy))
+                      @ [ last ]
             | [] -> yield [ 0uy ]
     }
     |> Seq.concat
     |> Seq.toList
 
-let moreBytesFollow  =
-   (&&&) 0x80uy >> (<>) 0uy
-   
+let moreBytesFollow =
+    (&&&) 0x80uy >> (<>) 0uy
+
 let calcByteVal i b =
-    (<<<) ((uint32 b) &&& 0x7fu) (i * 7) 
-    
-let decode (s:seq<byte>) =
-    let values = Seq.rev s |> Seq.mapi calcByteVal
-    values |> Seq.sum |> fun s -> Some [s]
-    // s |> Seq.takeWhile moreBytesFollow
+    (<<<) ((uint32 b) &&& 0x7fu) (i * 7)
+
+let lastByte = moreBytesFollow >> not
+
+// Taken from https://stackoverflow.com/a/6737659/180174
+let splitBy f input =
+    let i = ref 0
+    input
+    |> Seq.groupBy (fun x ->
+        if f x then incr i
+        !i)
+    |> Seq.map snd
+
+let decode (s: seq<byte>) =
+    let numbers = splitBy (fun n -> n &&& 0x80uy = 1uy) s
+    seq {
+        for n in numbers do
+            match n |> Seq.tryFind (lastByte) with
+            | None -> failwith "Non-terminating sequence"
+            | Some _ -> 
+                let values = Seq.rev n |> Seq.mapi calcByteVal
+                values |> Seq.sum
+    }
+    |> Seq.toList
+    |> Some
+// s |> Seq.takeWhile moreBytesFollow
