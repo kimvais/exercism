2a7b6b585b2dc2d0901650e06c2d8373cf85cb8a
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 01:46:18 +0200

    ++Elegance

--- largest-series-product/LargestSeriesProduct.fs
+++ largest-series-product/LargestSeriesProduct.fs
@@ -3,19 +3,15 @@
 open System
 
 let largestProduct (input: string) span =
-    match input with
-    | x when x |> String.exists (Char.IsDigit >> not) -> None
+    match (String.length input, span) with
+    | _ when input |> String.exists (Char.IsDigit >> not) -> None
+    | (x, y) when x < y || x < 0 || y < 0 -> None
+    | (0, x) when x > 0 -> None
+    | (_, 0) -> Some 1
     | _ ->
-
-        let l = String.length input
-        match (l, span) with
-        | (_, 0) -> Some 1
-        | (0, x) when x > 0 -> None
-        | (x, y) when x < y || x < 0 || y < 0 -> None
-        | _ ->
-            input
-            |> Seq.map (string >> int)
-            |> Seq.windowed span
-            |> Seq.map (Seq.reduce (*))
-            |> Seq.max
-            |> Some
+        input
+        |> Seq.map (string >> int)
+        |> Seq.windowed span
+        |> Seq.map (Seq.reduce (*))
+        |> Seq.max
+        |> Some
