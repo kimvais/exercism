b3a595f1be7fd0c70e2719c142f616b9fa37484e
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 00:33:23 +0200

    Sum of multiples.

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,18 @@
+﻿module SumOfMultiples
+
+let getMultiples limit n =
+    match n with
+    | 0 -> Seq.empty
+    | _ ->
+        seq {
+            let mulN x = n * !x
+            let m = ref 0
+            while mulN m < limit do
+                yield mulN m
+                incr m
+        }
+
+let sum (numbers: int list) (upperBound: int): int =
+    numbers
+    |> Seq.map (getMultiples upperBound)
+    |> (Seq.concat >> Seq.distinct >> Seq.sum)
