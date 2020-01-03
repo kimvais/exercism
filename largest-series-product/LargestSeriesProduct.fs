d4cc23e88e18cde13a0674e576ff012fe9ca3f8d
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 01:40:22 +0200

    Solved.

--- largest-series-product/LargestSeriesProduct.fs
+++ largest-series-product/LargestSeriesProduct.fs
@@ -1,3 +1,21 @@
 ﻿module LargestSeriesProduct
 
-let largestProduct input seriesLength : int option = failwith "You need to implement this function."
+open System
+
+let largestProduct (input: string) span =
+    match input with
+    | x when x |> String.exists (Char.IsDigit >> not) -> None
+    | _ ->
+
+        let l = String.length input
+        match (l, span) with
+        | (_, 0) -> Some 1
+        | (0, x) when x > 0 -> None
+        | (x, y) when x < y || x < 0 || y < 0 -> None
+        | _ ->
+            input
+            |> Seq.map (string >> int)
+            |> Seq.windowed span
+            |> Seq.map (Seq.reduce (*))
+            |> Seq.max
+            |> Some
