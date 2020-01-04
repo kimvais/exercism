72ce7e6723d4a5638af08203dad8e01560db8bd9
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 19:30:16 +0200

    Scrabble score.

---
+++ scrabble-score/ScrabbleScore.fs
@@ -0,0 +1,16 @@
+﻿module ScrabbleScore
+
+open System
+
+let scoreLetter = function
+    | 'a' | 'e' | 'i' | 'o' | 'u'| 'l' | 'n' | 'r' | 's' | 't' -> 1
+    | 'd' | 'g'                                                -> 2
+    | 'b' | 'c' | 'm' | 'p'                                    -> 3
+    | 'f' | 'h' | 'v' | 'w' | 'y'                              -> 4
+    | 'k'                                                      -> 5
+    | 'j' | 'x'                                                -> 8
+    | 'q' | 'z'                                                -> 10
+    | _                                                        -> 0
+
+let score: seq<char> -> int =
+    Seq.sumBy (Char.ToLower >> scoreLetter) 
