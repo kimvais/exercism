48478550d8d43f18816767f88d322c68e48ff2ab
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 23:00:03 +0200

    It works, but is ugly.

--- binary-search/BinarySearch.fs
+++ binary-search/BinarySearch.fs
@@ -1,3 +1,15 @@
 ﻿module BinarySearch
 
-let find input value = failwith "You need to implement this function."
+let rec search (input: 'a array) value start end' =
+    let middle = (start + end') / 2
+    if start < 0 || end' > Array.length input || start > end' then
+        None
+    else
+        match input.[middle] with
+        | s when s = value -> Some middle
+        | s when s > value -> search input value start (middle - 1)
+        | s when s < value -> search input value (middle + 1) end'
+        | _ -> None
+
+let find input value =
+    search input value 0 (Array.length input - 1)
