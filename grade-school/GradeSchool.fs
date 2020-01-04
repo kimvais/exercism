ae338f418fbf48ab98ebee3f3c83fb446f4216f8
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 16:15:32 +0200

    Solve grade school

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -1,11 +1,21 @@
 ﻿module GradeSchool
 
+open System
+
+
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School =
+    match school.TryFind grade with
+    | None -> school.Add(grade, [student])
+    | Some l -> school.Add(grade, (l @ [student]))
+    
+let roster (school :School): string list =
+    school |> (Map.toList >> List.map (snd >> List.sort) >> List.concat)
 
-let roster (school: School): string list = failwith "You need to implement this function."
-
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let grade (grade: int) (school: School): string list = 
+    match school.TryFind grade with
+    | None -> List.empty
+    | Some l -> l |> List.sort
