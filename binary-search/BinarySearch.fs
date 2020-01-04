e849bc6c0da485f25108c246f1005c90b283012a
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 23:06:40 +0200

    Cleanup.

--- binary-search/BinarySearch.fs
+++ binary-search/BinarySearch.fs
@@ -1,15 +1,13 @@
 ﻿module BinarySearch
 
-let rec search (input: 'a array) value start end' =
-    let middle = (start + end') / 2
-    if start < 0 || end' > Array.length input || start > end' then
-        None
-    else
-        match input.[middle] with
-        | s when s = value -> Some middle
-        | s when s > value -> search input value start (middle - 1)
-        | s when s < value -> search input value (middle + 1) end'
-        | _ -> None
-
-let find input value =
-    search input value 0 (Array.length input - 1)
+let find (input: 'a array) value =
+    let rec search value start end' =
+        if start > end' then
+            None
+        else
+            let middle = (start + end') / 2
+            match input.[middle] with
+            | s when s = value -> Some middle
+            | s when s > value -> search value start (middle - 1)
+            | s when s < value -> search value (middle + 1) end'
+    search value 0 (Array.length input - 1)
