8da50356f7223e5b57f15f37f68412d8ba27a2de
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-04 20:55:11 +0200

    Robot simulator unsolved.

---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,9 @@
+﻿module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+type Robot = { direction: Direction; position: Position }
+
+let create direction position = failwith "You need to implement this function."
+
+let move instructions robot = failwith "You need to implement this function."
