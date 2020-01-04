141d7a08fe79993fa25c639a67449f31121bc06e
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-05 00:44:58 +0200

    Hamming, solved.

---
+++ hamming/Hamming.fs
@@ -0,0 +1,11 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option =
+    if String.length strand1 = String.length strand2 then
+        Seq.zip strand1 strand2
+        |> Seq.sumBy (fun (a, b) ->
+            match a <> b with
+            | true -> 1
+            | false -> 0)
+        |> Some
+    else None
