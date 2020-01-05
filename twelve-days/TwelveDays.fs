0718fa929b42e43bd70e1f7079d8e4be72564e9f
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-05 20:35:51 +0200

    12 days, solved

--- twelve-days/TwelveDays.fs
+++ twelve-days/TwelveDays.fs
@@ -1,3 +1,30 @@
 ﻿module TwelveDays
 
-let recite start stop = failwith "You need to implement this function."
+let gifts =
+    [ "twelve Drummers Drumming, "
+      "eleven Pipers Piping, "
+      "ten Lords-a-Leaping, "
+      "nine Ladies Dancing, "
+      "eight Maids-a-Milking, "
+      "seven Swans-a-Swimming, "
+      "six Geese-a-Laying, "
+      "five Gold Rings, "
+      "four Calling Birds, "
+      "three French Hens, "
+      "two Turtle Doves, "
+      "and a Partridge in a Pear Tree." ]
+
+let cardinals =
+    [ "second"; "third"; "fourth"; "fifth"; "sixth"; "seventh"; "eighth"; "ninth"; "tenth"; "eleventh"; "twelfth" ]
+
+let getVerse n =
+    match n with
+    | 1 -> "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+    | _ ->
+        sprintf "On the %s day of Christmas my true love gave to me: %s" cardinals.[n - 2]
+            (gifts
+             |> Seq.skip (12 - n)
+             |> System.String.Concat)
+
+let recite start stop =
+    [ start .. stop ] |> List.map getVerse
