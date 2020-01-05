bc5d682975c897d56bbc4429b9d8b04700a1a2f1
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-05 20:00:29 +0200

    Secret handshake, solved.

---
+++ secret-handshake/SecretHandshake.fs
@@ -0,0 +1,13 @@
+﻿module SecretHandshake
+
+let operations = [ "wink"; "double blink"; "close your eyes"; "jump" ]
+
+let commands number =
+    let shake =
+        operations
+        |> List.indexed
+        |> List.filter (fun (i, x) -> (pown 2 i) &&& number > 0)
+        |> List.map (fun (_, x) -> x)
+    match number > 15 with
+    | false -> shake
+    | true -> shake |> List.rev
