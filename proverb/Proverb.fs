e5270d0cbe3f090828ba8a82236c604c88ce91e9
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-05 15:09:58 +0200

    Cleanup

--- proverb/Proverb.fs
+++ proverb/Proverb.fs
@@ -1,14 +1,14 @@
 ﻿module Proverb
 
-let makeVerse (l: string list) =
-    sprintf "For want of a %s the %s was lost." l.[0] l.[1]
-
-let makeLastVerse b =
-    sprintf "And all for the want of a %s." b
+let makeVerse (a, b) =
+    sprintf "For want of a %s the %s was lost." a b
+    
+let makeLastVerse =
+    sprintf "And all for the want of a %s."
 
 let recite (input: string list): string list =
-    match List.length input with
-    | 0 -> []
+    match input with
+    | [] -> []
     | _ ->
         let first = List.head input
-        (input |> List.windowed 2 |> List.map makeVerse) @ [ makeLastVerse first ]
+        (input |> List.pairwise |> List.map makeVerse) @ [ makeLastVerse first ]
