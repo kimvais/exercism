8a8764c549fed0db9584179fa50250bcd2a96d2f
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-05 15:04:06 +0200

    Solved proverb.

---
+++ proverb/Proverb.fs
@@ -0,0 +1,14 @@
+﻿module Proverb
+
+let makeVerse (l: string list) =
+    sprintf "For want of a %s the %s was lost." l.[0] l.[1]
+
+let makeLastVerse b =
+    sprintf "And all for the want of a %s." b
+
+let recite (input: string list): string list =
+    match List.length input with
+    | 0 -> []
+    | _ ->
+        let first = List.head input
+        (input |> List.windowed 2 |> List.map makeVerse) @ [ makeLastVerse first ]
