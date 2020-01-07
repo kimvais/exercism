722897e1dc8669c535c265edf3458cdefba1a8eb
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-07 18:58:01 +0200

    Word count, solved (I think)

---
+++ word-count/WordCount.fs
@@ -0,0 +1,10 @@
+﻿module WordCount
+
+open System.Text.RegularExpressions
+
+let word = Regex("[a-z]+'[a-z]+|[a-z0-9]+")
+
+let countWords (phrase: string) =
+    word.Matches(phrase.ToLower())
+    |> Seq.countBy (fun m -> m.Value)
+    |> Map.ofSeq
