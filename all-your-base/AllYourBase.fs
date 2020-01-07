3ff15ba3d83d6bd7da54693576d31b383c847fa4
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-07 22:21:02 +0200

    All your base, solved.

--- all-your-base/AllYourBase.fs
+++ all-your-base/AllYourBase.fs
@@ -1,3 +1,19 @@
 ﻿module AllYourBase
 
-let rebase digits inputBase outputBase = failwith "You need to implement this function."
+let rebase digits inputBase outputBase =
+    let unfolder baseN n =
+        match n with
+        | x when x <= 0 -> None
+        | _ -> Some(n % baseN, n / baseN)
+    match Seq.forall (fun n -> n >= 0 && n < inputBase) digits && inputBase > 1 && outputBase > 1 with
+    | false -> None
+    | true ->
+        Seq.rev digits
+        |> Seq.mapi (fun i d -> d * pown inputBase i)
+        |> Seq.sum
+        |> Seq.unfold (unfolder outputBase)
+        |> Seq.rev
+        |> Seq.toList
+        |> function
+        | [] -> Some [ 0 ]
+        | x -> Some x
