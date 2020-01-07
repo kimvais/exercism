dfac81c133661cbef95c10f3a92e9eb7639972d2
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-07 19:35:00 +0200

    Atbash cipher, solved.

---
+++ atbash-cipher/AtbashCipher.fs
@@ -0,0 +1,24 @@
+﻿module AtbashCipher
+
+let Cipher =
+    [ int 'z' .. -1 .. int 'a' ]
+    |> List.map char
+    |> Array.ofList
+
+let transmuteChar (c: char) =
+    let i = int (System.Char.ToLower(c)) - int 'a' 
+    match c with
+    | c when System.Char.IsLetter c -> Cipher.[i]
+    | c when System.Char.IsDigit c -> c
+
+// Since (en|de)coding is symmetric with extra steps in encoding, let's define decode first and re-use that in encode
+let decode =
+    String.filter (System.Char.IsLetterOrDigit) >> String.map transmuteChar
+
+let splitToGroupsOfFive: string -> string =
+    Seq.chunkBySize 5
+    >> Seq.map System.String.Concat
+    >> String.concat " "
+
+let encode =
+    decode >> splitToGroupsOfFive
