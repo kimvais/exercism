4d3c8808e3941b0b18bac1a856e6e4c74f35fe24
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-07 18:58:31 +0200

    ETL, solved.

---
+++ etl/Etl.fs
@@ -0,0 +1,15 @@
+﻿module Etl
+
+// Not very idiomatic functional code, but it works and is readable...
+
+let transform (input: Map<int, char list>): Map<char, int> =
+    let helper score letters =
+        seq {
+            for letter in letters do
+                yield (letter |> System.Char.ToLower, score)
+        }
+    seq {
+        for (s, ls) in input |> Map.toSeq do
+            yield! helper s ls
+    }
+    |> Map.ofSeq
