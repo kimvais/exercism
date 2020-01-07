9503f1a63e6bcabaf621ae95c55b652919a2f9d2
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-07 18:58:20 +0200

    Prime factors, completed.

---
+++ prime-factors/PrimeFactors.fs
@@ -0,0 +1,12 @@
+﻿module PrimeFactors
+
+let factors number =
+    let divisor = ref 2
+    seq {
+        let mutable remainder = number
+        while remainder > 1L do
+            if remainder % int64 !divisor = 0L then
+                remainder <- remainder / int64 !divisor
+                yield !divisor
+            else incr divisor
+    } |> Seq.toList
