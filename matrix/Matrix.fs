cdc7a39221477a6872173a20a1284ae2fa38bfd1
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-08 20:45:22 +0200

    Matrix, solved.

---
+++ matrix/Matrix.fs
@@ -0,0 +1,15 @@
+﻿module Matrix
+
+let make (s: string) =
+    s.Split('\n')
+    |> Array.map (fun s ->
+        (s.Split(' ')
+         |> Array.map int
+         |> Array.toList))
+    |> Array.toList
+
+let row index matrix =
+    (make matrix).[index - 1]
+
+let column index matrix =
+    make matrix |> List.map (fun row -> row.[index - 1])
