307582ca2a803ac60ee8696066cb1e5d2e071238
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-08 20:33:38 +0200

    Submitting copied solution :D

--- roman-numerals/RomanNumerals.fs
+++ roman-numerals/RomanNumerals.fs
@@ -1,48 +1,21 @@
 ﻿module RomanNumerals
 
-let matchDigit =
-    function
-    // These could be generated programmatically, but that's probably more code than defining them like this.
-    | 3000 -> "MMM"
-    | 2000 -> "MM"
-    | 1000 -> "M"
-    | 900 -> "CM"
-    | 800 -> "DCCC"
-    | 700 -> "DC"
-    | 600 -> "DC"
-    | 500 -> "D"
-    | 400 -> "CD"
-    | 300 -> "CCC"
-    | 200 -> "CC"
-    | 100 -> "C"
-    | 90 -> "XC"
-    | 80 -> "LXXX"
-    | 70 -> "LXX"
-    | 60 -> "LX"
-    | 50 -> "L"
-    | 40 -> "XL"
-    | 30 -> "XXX"
-    | 20 -> "XX"
-    | 10 -> "X"
-    | 9 -> "IX"
-    | 8 -> "VIII"
-    | 7 -> "VII"
-    | 6 -> "VI"
-    | 5 -> "V"
-    | 4 -> "IV"
-    | 3 -> "III"
-    | 2 -> "II"
-    | 1 -> "I"
+// Solution copied from pawelficher as his is so much more elegant than my original one :D
+// https://exercism.io/tracks/fsharp/exercises/roman-numerals/solutions/fa9a7f74fa4444a1bd68ddee15e80791
+let rec roman = function
+    | n when n >= 1000 -> "M" + roman (n - 1000)
+    | n when n >= 900 -> "CM" + roman (n - 900)
+    | n when n >= 500 -> "D" + roman (n - 500)
+    | n when n >= 400 -> "CD" + roman (n - 400)
+    | n when n >= 100 -> "C" + roman (n - 100)
+    | n when n >= 90 -> "XC" + roman (n - 90)
+    | n when n >= 50 -> "L" + roman (n - 50)
+    | n when n >= 40 -> "XL" + roman (n - 40)
+    | n when n >= 10 -> "X" + roman (n - 10)
+    | n when n >= 9 -> "IX" + roman (n - 9)
+    | n when n >= 5 -> "V" + roman (n - 5)
+    | n when n >= 4 -> "IV" + roman (n - 4)
+    | n when n >= 1 -> "I" + roman (n - 1)
     | 0 -> ""
-    | _ -> failwith "Can not convert to roman numeral."
+    | x -> failwith (sprintf "Invalid numeral %d" x)
 
-let roman number =
-    let unfolder n =
-        match n with
-        | x when x <= 0 -> None
-        | _ -> Some(n % 10, n / 10)
-    Seq.unfold unfolder number
-    |> Seq.mapi (fun i n -> n * (pown 10 i))
-    |> Seq.rev
-    |> Seq.map matchDigit
-    |> System.String.Concat
