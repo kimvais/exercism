b2909095c5eae96415df22a4af47378f21e538b2
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-09 18:53:07 +0200

    Solved simple cipher, dirty hack.

--- simple-cipher/SimpleCipher.fs
+++ simple-cipher/SimpleCipher.fs
@@ -1,11 +1,42 @@
 ﻿module SimpleCipher
 
+open System.Security.Cryptography
+
+
+let rng min max = RandomNumberGenerator.GetInt32(min, max)
+let keyLen = 100
+
+let makeRandomKey =
+    [ 1 .. keyLen ]
+    |> Seq.map (fun _ -> char (rng (int 'a') (int 'z') + 1))
+    |> System.String.Concat
+
+let offset = int 'a'
+
+let transcodeChar f (k: char) (c: char) =
+    let c' = f (int c - offset) (int k - offset)
+    // Need to handle this because modulo of negative number gives incorrectly a negative number
+    match c' with
+    | x when x < 0 -> char (offset + ((x + 26) % 26))
+    | x -> char (offset + (x % 26))
+
+let getKeyChar (k: string) i =
+    k.[i % String.length k]
+
+let transcode f k t = 
+    let getKey = getKeyChar k
+    t |> String.mapi (fun i c -> f (getKey i) c)
+    
+let encode = transcode (transcodeChar (+))
+
+let decode = transcode (transcodeChar (-))
+
 type SimpleCipher(key: string) =
-    
-    member __.Key with get() = failwith "You need to implement this function."
-    
-    member __.Encode(plaintext) = failwith "You need to implement this function."
-    
-    member __.Decode(ciphertext) = failwith "You need to implement this function."
-    
-    new() = failwith "You need to implement this function."
+
+    member val Key = key
+
+    member __.Encode(plaintext) = encode key plaintext
+
+    member __.Decode(ciphertext) = decode key ciphertext
+
+    new() = SimpleCipher(makeRandomKey)
