1ee7880ce3f0ed73d57f1f1aaa0fb3f83ec89357
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-12 13:01:40 +0200

    Naive algorithm that works for all sensible sets of denominations.

--- change/Change.fs
+++ change/Change.fs
@@ -1,3 +1,22 @@
 ﻿module Change
 
-let findFewestCoins coins target = failwith "You need to implement this function."
+let findFewestCoins coins target =
+    let getChange =
+        seq {
+            let mutable toGo = target
+            while toGo > 0 do
+                let coin =
+                    coins
+                    |> Seq.filter (fun x -> x <= toGo)
+                    |> Seq.max
+                toGo <- toGo - coin
+                yield coin
+        }
+    match target with
+    | x when x < 0 -> None
+    | x when x < (coins |> Seq.min) -> None
+    | _ ->
+        getChange
+        |> Seq.sort
+        |> Seq.toList
+        |> Some
