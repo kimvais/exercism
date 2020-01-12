bbca22b7def1d85b6056632cfc9b73e0eacdd2f6
Kimmo Parviainen-Jalanko <k@77.fi>
2020-01-12 12:42:44 +0200

    RLE, solved.

--- run-length-encoding/RunLengthEncoding.fs
+++ run-length-encoding/RunLengthEncoding.fs
@@ -1,5 +1,25 @@
 ﻿module RunLengthEncoding
 
-let encode input = failwith "You need to implement this function."
+open System.Text.RegularExpressions
 
-let decode input = failwith "You need to implement this function."
+let encodeOne (m: Match) =
+    let len = String.length m.Value
+    let c = m.Value.[0]
+    match len with
+    | 1 -> string c
+    | _ -> sprintf "%d%c" len c
+
+let decodeOne (m: Match) =
+    let len = m.Groups.[1].Value
+    let c = m.Groups.[2].Value
+    match len with
+    | "" -> string c
+    | _ -> String.init (int len) (fun _ -> c)
+
+let transform regex mapfun input =
+    Regex(regex).Matches(input)
+    |> Seq.map mapfun
+    |> System.String.Concat
+
+let encode = transform "((.)\2*)" encodeOne
+let decode = transform "(\d*)(\D)" decodeOne
