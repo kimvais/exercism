2cc054267f79362130c7bd0d2c73299b2a37c2b5
Kimmo Parviainen-Jalanko <kimvais@kimva.is>
2021-05-27 09:28:56 +0300

    List-ops

---
+++ list-ops/ListOps.fs
@@ -0,0 +1,17 @@
+﻿module ListOps
+
+let rec foldl = List.fold
+
+let rec foldr folder state list = List.foldBack folder list state
+
+let length = List.length
+
+let reverse = List.rev
+
+let map = List.map
+
+let filter = List.filter
+
+let append = List.append
+
+let concat = List.concat
